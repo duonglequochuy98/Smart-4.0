b788f0ff670669d1b1397be22040b6bac0ed1dc5
duonglequochuy98 <duonglequochuy98@gmail.com>
2026-01-19 10:01:02 +0700

    Update AIAssistant.tsx

--- components/AIAssistant.tsx
+++ components/AIAssistant.tsx
@@ -1,4 +1,3 @@
-
 import React, { useState, useRef, useEffect } from 'react';
 import { Message } from '../types';
 import { geminiService } from '../services/geminiService';
@@ -271,3 +270,4 @@ export const AIAssistant: React.FC<AIAssistantProps> = ({ onBack }) => {
     </div>
   );
 };
+
